95fa72c8c01f4ba1d4f2b57a903eabc77f6a31d1
Matthew Nielsen <matthewnielsen27@gmail.com>
2018-11-06 15:36:44 -0500

    initial commit

---
+++ lib/breadcrumbs.ex
@@ -0,0 +1,18 @@
+defmodule Breadcrumbs do
+  @moduledoc """
+  Documentation for Breadcrumbs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Breadcrumbs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/breadcrumbs_test.exs
@@ -0,0 +1,8 @@
+defmodule BreadcrumbsTest do
+  use ExUnit.Case
+  doctest Breadcrumbs
+
+  test "greets the world" do
+    assert Breadcrumbs.hello() == :world
+  end
+end
